cac80bb0c931a50d8698e0cb6d770cf2104eb3fc
Reynaldo Hermawan <reyhermawan97@gmail.com>
2019-02-11 01:57:04 -0800

    Weapon refactoring
    Possible design on weapon architecture to make it scalable

--- Group Project/Assets/Scripts/PlayerController.cs
+++ Group Project/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -49,6 +49,8 @@ public class PlayerController : MonoBehaviour
     private GameObject weaponFired;
     private GameObject environmentalDamage;
     private float gameEndDelay = 3f;
+
+    private WeaponScript equipedWeapon = null;
 
     private bool DEBUG = false;
 
@@ -384,17 +386,10 @@ public class PlayerController : MonoBehaviour
             {
                 //pickup
                 item.gameObject.transform.parent = equip.transform;
-                item.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
-                item.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-                item.GetComponent<Rigidbody2D>().angularVelocity = 0;
-                item.gameObject.transform.eulerAngles = equip.transform.eulerAngles;
-                item.gameObject.transform.localScale = new Vector3(1, transform.localScale.y, transform.localScale.z);
-                item.gameObject.transform.position = equip.transform.position;
-                item.transform.GetChild(1).GetChild(0).gameObject.SetActive(false);
-                if (item.name.Contains("Flashlight"))
-                {
-                    singleFire = true;
-                }
+
+                // Find the weapon script attached to the weapon (All weapons must extend WeaponScript)
+                equipedWeapon = this.GetComponentInChildren<WeaponScript>();
+                equipedWeapon.setPickupDefaults();
 
                 // Set object's isEquipped to true
                 item.gameObject.GetComponent<PickupController>().SetEquipped(true);
@@ -405,34 +400,25 @@ public class PlayerController : MonoBehaviour
         }
     }
 
+    private void fireWeapon()
+    {
+        if (dead || controlsDisabled || equipedWeapon == null)
+        {
+            return;
+        }
+        equipedWeapon.shootProjectile();
+    }
+
     private Transform dropObject()
     {
         //drop
-        if (equip.transform.childCount == 0) {
+        if (equipedWeapon == null) {
             return null;
         }
-        Transform dropped = equip.transform.GetChild(0);
-        dropped.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-        dropped.gameObject.transform.eulerAngles = Vector3.zero;
-        float s = -1;
-        if (transform.localScale.x == 1)
-        {
-            s = 1;
-        }
-        dropped.gameObject.transform.localScale = new Vector3(s, dropped.gameObject.transform.localScale.y, dropped.gameObject.transform.localScale.z);
-        dropped.parent = GameControl.instance.pickups.transform;
-        dropped.transform.GetChild(1).GetChild(0).gameObject.SetActive(true);
-        if (dropped.name.Contains("Flashlight"))
-        {
-            dropped.transform.GetChild(0).GetChild(0).GetChild(0).gameObject.SetActive(false);
-            dropped.transform.GetChild(1).GetChild(0).gameObject.SetActive(true);
-            dropped.GetComponent<Rigidbody2D>().gravityScale = 5;
-        }
-        if (dropped.name.Contains("Flamethrower") || dropped.name.Contains("Squirt Gun"))
-        {
-            dropped.transform.GetChild(0).GetChild(1).gameObject.GetComponent<ParticleSystem>().Stop();
-        }
-        return dropped;
+
+        equipedWeapon.setDroppedDefaults();
+
+        return equipedWeapon;
     }
 
     private void checkThrow()
@@ -443,7 +429,7 @@ public class PlayerController : MonoBehaviour
         }
         if (Input.GetButtonDown(tAxis))
         {
-            Transform dropped = null;
+            Transform dropped = null; //Maybe make a boolean to ropObject to see if throw or not?
             if (equip.transform.childCount == 1)
             {
                 dropped = dropObject();
@@ -619,111 +605,6 @@ public class PlayerController : MonoBehaviour
             color = "Red";
         }
         return color;
-    }
-
-    private void fireWeapon()
-    {
-        if (dead || controlsDisabled)
-        {
-            return;
-        }
-        if (Input.GetButtonDown(fAxis))
-        {
-            firing = true;
-            singleFire = true;
-        }
-        if (Input.GetButtonUp(fAxis))
-        {
-            firing = false;
-        }
-        if (singleFire)
-        {
-            fireOnce();
-        }
-        else
-        {
-            fireMulti();
-        }
-    }
-
-    private void fireOnce()
-    {
-        if(equip.transform.childCount != 1)
-        {
-            return;
-        }
-        if (firing && singleFire)
-        {
-            GameObject projectile = equip.transform.GetChild(0).gameObject;
-            if (equip.transform.GetChild(0).name.Contains("Flashlight"))
-            {
-                projectile = equip.transform.GetChild(0).transform.GetChild(0).GetChild(0).GetChild(0).gameObject;
-                projectile.SetActive(!projectile.activeSelf);
-            }
-            singleFire = false;
-        }
-    }
-    private void fireMulti()
-    {
-        if (equip.transform.childCount == 0)
-        {
-            return;
-        }
-        if (equip.transform.GetChild(0).name.Contains("Flashlight"))
-        {
-            return;
-        }
-        GameObject projectile = equip.transform.GetChild(0).gameObject; 
-        projectile = equip.transform.GetChild(0).transform.GetChild(0).GetChild(1).gameObject;
-        ParticleSystem ps = projectile.GetComponent<ParticleSystem>();
-        if (firing)
-        {
-            if (!ps.isEmitting)
-            {
-                ps.Play();
-            }
-        }
-        else
-        {
-            if (ps.isEmitting)
-            {
-                ps.Stop();
-            }
-        }
-    }
-
-    private void checkDamage()
-    {
-        if(particleParent == null || particleParent.name == gameObject.name)
-        {
-            return;
-        }
-        if (particleParent != environmentalDamage)
-        {
-            if (damaged)
-            {
-                health -= GameControl.instance.flamethrowerDamage * Time.deltaTime;
-                healthBar.value = health;
-                damaged = false;
-                if (health <= 0)
-                {
-                    dead = true;
-                }
-            }
-        }
-        else
-        {
-            if (damaged)
-            {
-                health -= GameControl.instance.avalancheDamage * Time.deltaTime;
-                healthBar.value = health;
-                damaged = false;
-                if (health <= 0)
-                {
-                    dead = true;
-                }
-            }
-        }
     }
 
     private void die()
---
+++ Group Project/Assets/Scripts/WeaponScriptHelper.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class WeaponScriptHelper
+{
+    public static void setDroppedDefaults(this WeaponScript weapon){
+        // Do stuff
+        weapon.setUniqueDroppedDefaults();
+    }
+}
