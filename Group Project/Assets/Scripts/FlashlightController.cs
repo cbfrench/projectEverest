166b546c8ac1ca437bbf66b9a84ce407f20a2975
Caleb Biggers <calebbiggers@gmail.com>
2019-02-14 09:25:19 -0800

    Weapon Update
    - Created player prefab
    - Added old prefabs folder
    - Started converting current weapons to use new scripts

---
+++ Group Project/Assets/Scripts/FlashlightController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashlightController : MonoBehaviour, WeaponScript
+{
+
+    public GameObject bulb; // Reference to bulb of flashlight
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulb.SetActive(false);
+    }
+
+    // Called to shoot weapon
+    public void Shoot()
+    {
+        // Toggle bulb
+        bulb.SetActive(!bulb.activeSelf);
+    }
+
+    public void Stop()
+    {
+        // Nothing needed here
+    }
+}
