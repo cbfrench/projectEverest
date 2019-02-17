214c6a790f9c4bf1787f9b1cf811778ef681bd4f
calebbiggers <calebbiggers@gmail.com>
2019-02-16 19:38:26 -0800

    Grenade Launcher
    - Added grenade launcher

---
+++ Group Project/Assets/Scripts/GrenadeController.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeController : MonoBehaviour
+{
+    public AudioClip explosionSound;
+    public float launchForce = 2f;
+    public float lifeSpan = 1f;
+    public float explosionRadius = 2.5f;
+    public float explosionForce = 300f;
+    public int bounceLimit = 3;
+    public GameObject explosion;
+
+    private Rigidbody2D rb2d;
+    private ParticleSystem particles;
+    private float lifeTime = 0;
+    private int bounces;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        particles = GetComponent<ParticleSystem>();
+
+        rb2d.AddForce(new Vector2(launchForce, 0));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Check for life span
+        lifeTime += Time.deltaTime;
+        if(lifeTime >= lifeSpan)
+        {
+            Detonate();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Detonate();
+        }
+        else if(collision.gameObject.tag == "Platforms" || collision.gameObject.tag == "Wall")
+        {
+            bounces++;
+            if(bounces >= bounceLimit)
+            {
+                Detonate();
+            }
+        }
+    }
+
+    public void Detonate()
+    {
+        // Play the explosion
+        particles.Play();
+        AudioSource.PlayClipAtPoint(explosionSound, Camera.main.transform.position);
+
+        // Set velocity to zero
+        GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        // Get explosion hits
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(new Vector2(transform.position.x, transform.position.y), explosionRadius);
+        foreach (Collider2D hit in colliders)
+        {
+            Rigidbody2D rb2d = hit.GetComponent<Rigidbody2D>();
+
+            if (rb2d != null)
+            {
+                rb2d.AddForce(new Vector2(hit.transform.position.x - transform.position.x, hit.transform.position.y - transform.position.y) * explosionForce);
+            }
+
+            //Debug.Log(hit.gameObject.tag);
+            if (hit.gameObject.tag == "Player")
+            {
+                // Damage player here
+            }
+        }
+
+        // Spawn indicator
+        Instantiate(explosion, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
+
+        // Destory the grenade
+        Destroy(gameObject);
+    }
+}
---
+++ Group Project/Assets/Scripts/GrenadeLauncherController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GrenadeLauncherController : MonoBehaviour, WeaponScript
+{
+    public Text label;          // Reference to the text label
+    public GameObject grenadePrefab;
+    public Transform grenadeSpawn;
+    private GameObject player;          // Stores reference to the player
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // Initialize 
+        player = null;
+
+        // Set label text
+        label.text = "Grenade Launcher";
+        label.gameObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Called when a player picks up the weapon
+    public void initWeaponUnique(GameObject player)
+    {
+        // Set the player reference
+        this.player = player;
+        label.gameObject.SetActive(false);
+    }
+
+    // Called when a player drops the weapon
+    public void resetWeaponUnique(GameObject player)
+    {
+        // Set the player reference back to null on drop
+        this.player = null;
+        label.gameObject.SetActive(true);
+    }
+
+    // Shoot flame
+    public void shoot()
+    {
+        Instantiate(grenadePrefab, new Vector3(grenadeSpawn.position.x, grenadeSpawn.position.y, grenadeSpawn.position.z), transform.rotation);
+    }
+
+    // Stop shooting flame
+    public void stop()
+    {
+
+    }
+}
