601e6769ee39dc03b215775c81aea343f23e5cbd
Connor French <cbfrench@calpoly.edu>
2019-03-17 20:37:25 -0700

    Updated controls menu to automatically change for Sony controllers

---
+++ Group Project/Assets/Scripts/Controls.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controls : MonoBehaviour
+{
+    /* Author: Connor French
+     * Description: changes controls text based on controller used
+     */
+    private Text t;
+    private bool sony = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = gameObject.GetComponent<Text>();
+        string[] controllers = Input.GetJoystickNames();
+        if (controllers.Length == 0)
+        {
+            return;
+        }
+        for (int i = 0; i < controllers.Length; i++)
+        {
+            if (!controllers[i].Contains("vJoy"))
+            {
+                sony = true;
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (sony)
+        {
+            t.text = "Move: Analog Stick\nJump: X\nEquip: Square\nFire: L2 / R2\nThrow: Circle";
+        }
+    }
+}
